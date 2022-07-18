15af0cc86d3b3d76f37873e95c29183f91e6c212
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-07-18 22:45:14 +0100

    add linters files

--- person.rb
+++ person.rb
@@ -1,26 +1,25 @@
 class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-def initialize(age, name = 'Unknown', parent_permission: true)
-  @id = Random.rand(1..100)
-  @name = name
-  @age = age
-  @parent_permission = parent_permission
-end
+  attr_accessor :name, :age
+  attr_reader :id
 
-attr_accessor :name, :age
-attr_reader :id
-
-def can_use_services?
+  def can_use_services?
     if is_of_age?
       true
     else
       false
     end
-end
+  end
 
-private
+  private
 
-def is_of_age?
-  @age >= 18
-end
+  def is_of_age?
+    @age >= 18
+  end
 end
